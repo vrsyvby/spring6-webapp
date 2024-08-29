fe0a3f35debc1764f140e1983b4066cd9e4e9aeb
Venkata Radheshyam Yekkirala <Vrsyvby@gmail.com>
2024-08-29 09:57:44 +0530

    GENAI=NO Adding domain objects to spring web app

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+}
