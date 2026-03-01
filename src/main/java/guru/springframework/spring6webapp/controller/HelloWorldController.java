4a19bda136648f274ab99fe768c086d058dd523d
Venkata Radheshyam Yekkirala <78905728+vrsyvby@users.noreply.github.com>
2026-03-01 22:35:40 +0530

    Update helloWorld method output message

--- src/main/java/guru/springframework/spring6webapp/controller/HelloWorldController.java
+++ src/main/java/guru/springframework/spring6webapp/controller/HelloWorldController.java
@@ -5,7 +5,7 @@ public class HelloWorldController {
 
     @GetMapping("/world")
     public String helloWorld() {
-        System.out.println("Hello World - Manoj-- again");
+        System.out.println("Hello World - Manoj-- again--not-again");
         return "Hello, World!";
     }
 }
