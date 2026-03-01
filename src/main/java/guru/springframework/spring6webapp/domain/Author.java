39d90207110a884a28579bdb99f72db183b2094e
Venkata Radheshyam Yekkirala <Vrsyvby@gmail.com>
2026-03-01 22:39:00 +0530

    name of the variable si changged

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -10,7 +10,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String firstName;
+    private String firstNameOfThePerson;
     private String lastName;
 
 }
