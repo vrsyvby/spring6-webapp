23387ce741cb6f3db1605579de668f929ba62d51
Venkata Radheshyam Yekkirala <Vrsyvby@gmail.com>
2026-03-01 22:00:45 +0530

    my first commit

---
+++ src/main/java/guru/springframework/spring6webapp/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6webapp.controller;
+
+@RestController("/hello")
+public class HelloWorldController {
+
+    @GetMapping("/world")
+    public String helloWorld() {
+        System.out.println("Hello World");
+        return "Hello, World!";
+    }
+}
